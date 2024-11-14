7ed85f789cceddea7047e2ea5e727fbe2586336e
Somies <misaelsoria789@gmail.com>
2024-11-14 11:12:38 -0500

    First commit

---
+++ VISTA/JS/evento.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const swiper = new Swiper('.swiper-container', {
+        loop: true,
+        slidesPerView: 1, 
+        spaceBetween: 20,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true, 
+        },
+        navigation: {
+            nextEl: '.swiper-button-next', 
+            prevEl: '.swiper-button-prev', 
+        },
+        breakpoints: {
+            668: {
+                slidesPerView: 2, 
+                spaceBetween: 20, 
+            },
+            1024: {
+                slidesPerView: 4, 
+                spaceBetween: 20, 
+            }
+        }
+    });
+});
