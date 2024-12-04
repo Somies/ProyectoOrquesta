f7838f2e67815ef31085be17159c21eb13d548aa
Somies <misaelsoria789@gmail.com>
2024-12-04 13:20:41 -0500

    Descripción breve de los cambios realizados

--- VISTA/JS/evento.js
+++ VISTA/JS/evento.js
@@ -1,25 +1,36 @@
 document.addEventListener("DOMContentLoaded", function () {
+    // Inicializar Swiper
     const swiper = new Swiper('.swiper-container', {
         loop: true,
-        slidesPerView: 1, 
+        slidesPerView: 1,
         spaceBetween: 20,
         pagination: {
             el: '.swiper-pagination',
-            clickable: true, 
+            clickable: true,
         },
         navigation: {
-            nextEl: '.swiper-button-next', 
-            prevEl: '.swiper-button-prev', 
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
         },
         breakpoints: {
             668: {
-                slidesPerView: 2, 
-                spaceBetween: 20, 
+                slidesPerView: 2,
+                spaceBetween: 20,
             },
             1024: {
-                slidesPerView: 4, 
-                spaceBetween: 20, 
+                slidesPerView: 4,
+                spaceBetween: 20,
             }
         }
+    });
+
+    // Agregar evento de clic a las imágenes
+    document.querySelectorAll('.card-item').forEach((item) => {
+        item.addEventListener('click', () => {
+            const badge = item.querySelector('.badge').textContent;
+            if (badge === 'MATRIMONIO') {
+                document.querySelector('.sub1cuerpo3').scrollIntoView({ behavior: 'smooth' });
+            }
+        });
     });
 });
